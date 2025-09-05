958ee0e342941d0c2b5c69464d9ed1e58f6001ad
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-09-05 19:54:15 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "excel-insights-qcu6e",
+  "appId": "1:371473431007:web:96672b4aface6e06296c6a",
+  "storageBucket": "excel-insights-qcu6e.firebasestorage.app",
+  "apiKey": "AIzaSyAfInve2z7YKMcEHe0UzSYils0jPFbQysU",
+  "authDomain": "excel-insights-qcu6e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371473431007"
 };
 
 // Initialize Firebase
